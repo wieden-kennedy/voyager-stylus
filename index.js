d262c977f04fe0f911fc60be3084f3734ab455c3
David Glivar <davidglivar@gmail.com>
2014-09-09 10:02:23 -0700

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+var voyager = require('voyager')
+  , csso = require('gulp-csso')
+  , plumber = require('gulp-plumber')
+  , stylus = require('gulp-stylus')
+  , vfs = require('vinyl-fs')
+  , CWD = process.cwd();
+
+voyager.task('stylus', function (done) {
+  vfs.src([
+      CWD + '/src/stylesheets/main.styl'
+    ])
+    .pipe(plumber())
+    .pipe(stylus({ errors: true }))
+    .pipe(vfs.dest(CWD + '/.dev/stylesheets'))
+    .on('end', done);
+});
